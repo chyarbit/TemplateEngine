993eeb682aa518cdb4c19b1ee44046bc6909229b
Cynthia Yuen <cynthia.h.yuen@gmail.com>
2020-03-28 13:21:51 -0500

    finished setting up inquirer questions

--- Develop/app.js
+++ Develop/app.js
@@ -1,17 +1,81 @@
+// pull in needed reference files using require
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-// Write code to use inquirer to gather information about the development team members,
+
+// define constant questions for the inquirer prompt
+const questions = [
+    {
+        type: "input",
+        message: "Please enter the name",
+        name: "name"
+    },
+
+    {
+        type: "input",
+        message: "Please enter the employee ID number",
+        name: "id"
+    },
+
+    {
+        type: "input",
+        message: "Please enter the email",
+        name: "email"
+    },
+
+    {
+        type: "rawlist",
+        message: "Please select the role",
+        choices: ["Manager", "Engineer", "Intern"],
+        name: "role"
+    },
+
+    {
+        type: "input",
+        message: "Please enter the office number",
+        name: "officeNumber",
+        when: function(answers){
+            return answers.role === "Manager";
+        }
+    },
+
+    {
+        type: "input",
+        message: "Please enter the gitHub username",
+        name: "gitHub",
+        when: function(answers){
+            return answers.role === "Engineer";
+        }
+    },
+
+    {
+        type: "input",
+        message: "Please enter the school name",
+        name: "school",
+        when: function(answers){
+            return answers.role === "Intern";
+        }
+    }
+]
+
+// use inquirer to gather information about the development team members by prompting the questions variable
+inquirer
+.prompt(questions)
+// use .then promise to open up a function and pass in the parameters as an object {name, id, email, role}
+.then(function({name, id, email, role}){
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+})
+
+
 ​
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
